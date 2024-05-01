c57e1da5739aba2816fd29e3cf122dc6c6ff4201
solahidris <emailsolah@gmail.com>
2024-05-02 00:19:35 +0800

    home

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,11 +1,140 @@
+import { Button } from "./ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 const HomePage = () => {
   console.log("homepage");
 
   return (
-    <div>
-      <p>hi homepage</p>
+    <div className="flex p-10">
+      {/* Left Side */}
+      <div className="flex flex-col gap-20 w-[10vw]">
+        <div className="flex flex-col bg-gray-300 rounded-3xl w-32 mx-auto justify-center items-center py-4">
+          <span>Awards</span>
+          <span className="text-xs pb-2">Coming Soon</span>
+        </div>
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          orientation="vertical"
+          className="w-full max-w-xs"
+        >
+          <CarouselContent className="-mt-1 h-[200px]">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index} className="pt-1 md:basis-1/2">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex items-center justify-center p-6">
+                      <span className="text-3xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+      {/* Left Side */}
+      <div className="flex flex-col gap-8 w-[90vw]">
+        <div className="flex justify-center">
+          <Carousel className="w-full max-w-sm">
+            <CarouselContent className="-ml-1">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem
+                  key={index}
+                  className="pl-1 md:basis-1/2 lg:basis-1/3"
+                >
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-2xl font-semibold">
+                          {index + 1}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+
+        <div className="flex gap-4 items-center justify-center">
+          <div className="flex py-1.5 px-4 bg-gray-300 rounded-full items-center gap-4 w-28">
+            <p className="font-bold pl-2">1</p>
+          </div>
+          <div className="flex py-1.5 px-4 bg-gray-300 rounded-full items-center gap-4 w-28">
+            <p className="font-bold pl-2">2</p>
+          </div>
+          <div className="flex py-1.5 px-4 bg-gray-300 rounded-full items-center gap-4 w-28">
+            <p className="font-bold pl-2">3</p>
+          </div>
+        </div>
+
+        <div className="flex justify-center">
+          <Carousel className="w-full max-w-md">
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index}>
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-4xl font-semibold">
+                          {index + 1}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+
+        <div className="flex justify-center">
+          <Carousel className="w-full max-w-sm">
+            <CarouselContent className="-ml-1">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem
+                  key={index}
+                  className="pl-1 md:basis-1/2 lg:basis-1/3"
+                >
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-2xl font-semibold">
+                          {index + 1}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default HomePage;
+
+// https://www.figma.com/file/CVhKjEoThe5bM8jF3ZeyFy/Memo?type=design&node-id=0-1&mode=design
--- components/NavigationHeader.tsx
+++ components/NavigationHeader.tsx
@@ -1,22 +1,34 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Label } from "./ui/label";
+import { Card } from "./ui/card";
 
 const NavigationHeader = () => {
   return (
-    <div className="flex flex-col fixed right-8 top-8 gap-2">
-      <Label> Page Navigation</Label>
-    <div className="flex gap-2">
-      <Link href="/" className="w-20 bg-black text-white rounded-lg text-center">
-        <Button>Home</Button>
-      </Link>
-      <Link href="/awards" className="w-20 bg-black text-white rounded-lg text-center">
-        <Button>Awards</Button>
-      </Link>
-      <Link href="/create" className="w-20 bg-black text-white rounded-lg text-center">
-        <Button>Create</Button>
-      </Link>
-    </div>
+    <div className="relative m-10 gap-2 ">
+      <Card className="p-3 bg-white flex items-center gap-4 z-[1000px]">
+        <Label>Page Navigation</Label>
+        <div className="flex gap-2">
+          <Link
+            href="/"
+            className="w-20 bg-black text-white rounded-lg text-center"
+          >
+            <Button>Home</Button>
+          </Link>
+          <Link
+            href="/awards"
+            className="w-20 bg-black text-white rounded-lg text-center"
+          >
+            <Button>Awards</Button>
+          </Link>
+          <Link
+            href="/create"
+            className="w-20 bg-black text-white rounded-lg text-center"
+          >
+            <Button>Create</Button>
+          </Link>
+        </div>
+      </Card>
     </div>
   );
 };
